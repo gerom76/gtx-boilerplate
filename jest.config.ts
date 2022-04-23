3b33ea35450cea6153bda6e2719debf68d303a98
Jaromin Sycz <jaromin.sycz.ext@siemens.com>
2022-04-23 12:33:40 +0200

    Added jest.confi.ts

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type {Config} from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/"
+  ]
+};
+export default config;
