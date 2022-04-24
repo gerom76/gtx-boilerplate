3035ae138c565a3646847385c196842d35802b32
Jaromin Sycz <jaromin.sycz.ext@siemens.com>
2022-04-24 22:46:07 +0200

    "baseUrl": "src"

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ComponentA } from './components/ComponentA';
+import { ComponentA } from 'components/ComponentA';
 
 function App() {
   const y = 1;
