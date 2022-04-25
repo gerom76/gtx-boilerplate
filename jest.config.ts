acdadf816788c009d7884e49a8450bfabfb6fb69
Jaromin Sycz <jaromin.sycz.ext@siemens.com>
2022-04-25 21:52:43 +0200

    Enabled test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,24 @@ import type { Config } from '@jest/types';
 
 // Sync object
 const config: Config.InitialOptions = {
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
   verbose: true,
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'cobertura', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
 };
 export default config;
